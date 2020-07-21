e52d32ce19f92e61751202cee42a831bc2bd38fd
Dev1LPanda <63203247+Dev1LPanda@users.noreply.github.com>
2020-07-21 19:17:11 +0300

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,17 @@
+<?php
+$token = "1306655410:AAEfStmp61kHQA8DFPlvMskSPZmpybG_CU8";
+$bot = new \TelegramBot\Api\Client($token);
+// команда для start
+$bot->command('start', function ($message) use ($bot) {
+    $answer = 'Добро пожаловать!';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+// команда для помощи
+$bot->command('help', function ($message) use ($bot) {
+    $answer = 'Команды:
+/help - вывод справки';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+$bot->run();
